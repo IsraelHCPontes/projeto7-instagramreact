e78707afe0bf6c50eceb1b127027780839575b43
israelHCPontes <israelhcpontes@gmail.com>
2022-07-15 11:34:43 -0300

    modelo refeito

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Story from "./Story"
+
+export default function App(){
+    return (
+            <div class="container">
+                <div class="containerStory">
+                    <div class="containerEsquerda">
+                        <Story img ="./arquivosulteis/img/9gag.svg"/>
+                    </div>
+                </div>    
+            </div>
+    )
+}
---
+++ src/Story.js
@@ -0,0 +1,11 @@
+export default function Story(props){
+    return (
+        <div>
+            <div class="story">
+                    <div class="imagem">
+                        <img src={props.img}/ >
+                    </div>
+             </div>
+        </div>
+    );
+}
